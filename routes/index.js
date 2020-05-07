2ca6f3d1d38afe979cc7f58433da1bffdc56a82e
yanghyeonjin <guswls7081@gmail.com>
2020-05-07 18:01:59 +0900

    단일기기 푸시 알림 전송

--- routes/index.js
+++ routes/index.js
@@ -14,4 +14,33 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* 단일 기기 PUSH 전송*/
+router.post("/ajax/sendNotificationOne", function (req, res, next) {
+  // This registration token comes from the client FCM SDKs.
+  const registrationToken = "";
+
+  const message = {
+    notification: {
+      title: "Test",
+      body: "Test Push!"
+    },
+    data: {
+      score: '850',
+      time: '2:45'
+    },
+    token: registrationToken
+  }
+
+  // Send a message to the device corresponding to the provided
+  // registration token.
+  firebaseAdmin.messaging().send(message)
+    .then((response) => {
+      // Response is a message ID string.
+      res.json(response);
+    })
+    .catch((error) => {
+      console.log('Error sending message:', error)
+    })
+})
+
 module.exports = router;
