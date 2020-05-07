e86b799418cb060c80090e7198cbc2d872746fc0
yanghyeonjin <guswls7081@gmail.com>
2020-05-07 15:50:49 +0900

    Firebase Admin SDK 설정

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+const firebaseAdmin = require('firebase-admin');
+const serviceAccount = require("../config/firebase-adminsdk.json");
+
+/* Firebase Admin SDK 초기화 */
+firebaseAdmin.initializeApp({
+  credential: firebaseAdmin.credential.cert(serviceAccount)
+})
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
