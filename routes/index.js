a4a245194f4a27e557afb33af0325df759a25bad
yanghyeonjin <guswls7081@gmail.com>
2020-05-08 15:45:04 +0900

    백그라운드 notification 이슈 해결

--- routes/index.js
+++ routes/index.js
@@ -19,7 +19,7 @@ router.get('/', function (req, res, next) {
 router.post("/ajax/sendNotificationOne", function (req, res, next) {
   const destination = req.body.destination; // 푸시알림 클릭 시 목적지
 
-  // 목적지 액티비티 (백그라운드에서 작동하기 위해)
+  // 목적지 액티비티
   let destActivity = "";
   if (destination == "page1") {
     destActivity = "PAGE_1_ACTIVITY"
@@ -41,21 +41,15 @@ router.post("/ajax/sendNotificationOne", function (req, res, next) {
       // This registration token comes from the client FCM SDKs.
       let registrationToken = rows[0].token;
 
+      // notification 키를 사용하여 전달했을 때, 백그라운드 알림 이슈 있음.
+      // data 키로 모든 값을 전달해서 해결.
       const message = {
-        notification: {
-          title: "FCM Notification Example",
-          body: `GO TO ${destination}`
-        },
         data: {
+          title: "FCM Notification Example",
+          body: `GO TO ${destination}`,
           destination: destination
         },
-        token: registrationToken,
-        android: {
-          notification: {
-            sound: "default",
-            click_action: destActivity // 백그라운드에서 작동하기 위해
-          }
-        }
+        token: registrationToken
       }
 
       // Send a message to the device corresponding to the provided
